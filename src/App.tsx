2d2e654bdd80db90d3a92dea678bdb0ed37474be
AR-Classes <todbuf@me.fastbolt.vip>
2025-12-03 12:44:59 +0000

    changed base url

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/arclasses-bright-future">
         <Layout>
           <Routes>
             <Route path="/" element={<Index />} />
